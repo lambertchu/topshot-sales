15a22b764905cc5946a1aec0614c23e6bae1e287
Lambert Chu <lambert.chu@gmail.com>
2020-12-16 00:14:51 -0500

    Save moment data as csv

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"encoding/csv"
+
 	"context"
 	"fmt"
 	"github.com/rrrkren/topshot-sales/topshot"
@@ -13,6 +16,20 @@ func handleErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func writeCsv(data [][]string) {
+    file, err := os.Create("result.csv")
+    handleErr(err)
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    for _, value := range data {
+        err := writer.Write(value)
+        handleErr(err)
+    }
 }
 
 func main() {
@@ -30,22 +47,43 @@ func main() {
 	// fetch block events of topshot Market.MomentPurchased events for the past 1000 blocks
 	blockEvents, err := flowClient.GetEventsForHeightRange(context.Background(), client.EventRangeQuery{
 		Type:        "A.c1e4f4f4c4257510.Market.MomentPurchased",
-		StartHeight: latestBlock.Height - 1000,
+		StartHeight: latestBlock.Height - 10000,
 		EndHeight:   latestBlock.Height,
 	})
 	handleErr(err)
 
+	// Build CSV of fetched data
+	var header = []string{"ID", "Price", "Seller", "SerialNumber", "SetID", "SetName", "PlayID", "PlayName", "TransactionID", "BlockHeight"}
+	var data = [][]string {header}
+
 	for _, blockEvent := range blockEvents {
 		for _, purchaseEvent := range blockEvent.Events {
 			// loop through the Market.MomentPurchased events in this blockEvent
+			var row = []string{}
+
+			// Purchase Event
 			e := topshot.MomentPurchasedEvent(purchaseEvent.Value)
-			fmt.Println(e)
+			row = append(row, fmt.Sprint(e.Id()))
+			row = append(row, fmt.Sprint(e.Price()))
+			row = append(row, fmt.Sprint(e.Seller()))
+
 			saleMoment, err := topshot.GetSaleMomentFromOwnerAtBlock(flowClient, blockEvent.Height-1, *e.Seller(), e.Id())
 			handleErr(err)
-			fmt.Println(saleMoment)
-			fmt.Printf("transactionID: %s, block height: %d\n",
-				purchaseEvent.TransactionID.String(), blockEvent.Height)
-			fmt.Println()
+
+			// Sale Moment
+			row = append(row, fmt.Sprint(saleMoment.SerialNumber()))
+			row = append(row, fmt.Sprint(saleMoment.SetID()))
+			row = append(row, fmt.Sprint(saleMoment.SetName()))
+			row = append(row, fmt.Sprint(saleMoment.PlayID()))
+			row = append(row, fmt.Sprint(saleMoment.Play()["FullName"]))
+
+			// Transaction Info
+			row = append(row, purchaseEvent.TransactionID.String())
+			row = append(row, fmt.Sprint(blockEvent.Height))
+
+			data = append(data, row)
 		}
 	}
+
+	writeCsv(data)
 }
